1ed248a63c514cfdf8e4e61385b3e9dccbf7658b
Ole Tange <ole@tange.dk>
2018-12-02 14:46:50 +0100

    parallel: Better /proc/cpuinfo parser.

--- testsuite/tests-to-run/parallel-local-0.3s.sh
+++ testsuite/tests-to-run/parallel-local-0.3s.sh
@@ -865,6 +865,7 @@ par_space_envvar() {
     export PARALLEL=" -v" && parallel echo ::: 'space in envvar OK'
 }
 
+
 export -f $(compgen -A function | grep par_)
 compgen -A function | grep par_ | LC_ALL=C sort |
     parallel -j6 --tag -k --joblog +/tmp/jl-`basename $0` '{} 2>&1'
