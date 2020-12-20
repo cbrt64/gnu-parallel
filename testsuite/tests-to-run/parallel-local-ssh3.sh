2a600a97be0f2d35bbb7edc5546343f42da845e0
Ole Tange <ole@tange.dk>
2020-12-20 23:24:34 +0100

    parallel: {hgrp} = intersection of the hgrp of job and sshlogin.

--- testsuite/tests-to-run/parallel-local-ssh3.sh
+++ testsuite/tests-to-run/parallel-local-ssh3.sh
@@ -1,58 +1,81 @@
 #!/bin/bash
 
-
 # SSH only allowed to localhost/lo
-cat <<'EOF' | parallel -vj100% --retries 3 -k --joblog /tmp/jl-`basename $0` -L1 -r
-echo '### trailing space in sshlogin'
-  echo 'sshlogin trailing space' | parallel  --sshlogin "ssh -l parallel localhost " echo
 
-echo '### Special char file and dir transfer return and cleanup'
-  cd /tmp; 
-  mkdir -p d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"; 
-  echo local > d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/f"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"; 
-  ssh parallel@lo rm -rf d'*'/; 
-  mytouch() { 
-    cat d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/f"`perl -e 'print pack("c*",1..9,11..46,48..255)'`" > d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/g"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"; 
-    echo remote OK >> d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/g"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"; 
-  }; 
-  export -f mytouch; 
-  parallel --env mytouch -Sparallel@lo --transfer 
-    --return {=s:/f:/g:=} 
-    mytouch 
-    ::: d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/f"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"; 
-  cat d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/g"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"
+par_trailing_space_sshlogin() {
+    echo '### trailing space in sshlogin'
+    echo 'sshlogin trailing space' |
+	parallel  --sshlogin "ssh -l parallel localhost " echo
+}
 
-echo '### Uniq {=perlexpr=} in return - not used in command'
-  cd /tmp; 
-  rm -f /tmp/parallel_perlexpr.2Parallel_PerlexPr; 
-  echo local > parallel_perlexpr; 
-  parallel -Sparallel@lo --trc {=s/pr/pr.2/=}{=s/p/P/g=} echo remote OK '>' {}.2{=s/p/P/g=} ::: parallel_perlexpr; 
-  cat /tmp/parallel_perlexpr.2Parallel_PerlexPr; 
-  rm -f /tmp/parallel_perlexpr.2Parallel_PerlexPr /tmp/parallel_perlexpr
+par_special_char_trc() {
+    echo '### Special char file and dir transfer return and cleanup'
+    cd /tmp
+    mkdir -p d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"
+    echo local > d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/f"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"
+    ssh parallel@lo rm -rf d'*'/
+    mytouch() {
+	cat d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/f"`perl -e 'print pack("c*",1..9,11..46,48..255)'`" > d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/g"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"
+	echo remote OK >> d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/g"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"
+    }
+    export -f mytouch
+    parallel --env mytouch -Sparallel@lo --transfer --return {=s:/f:/g:=} mytouch \
+	     ::: d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/f"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"
+    cat d"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"/g"`perl -e 'print pack("c*",1..9,11..46,48..255)'`"
+    # TODO Should be changed to --return '{=s:/f:/g:=}' and tested with csh - is error code kept?
+}
 
-#  Should be changed to --return '{=s:/f:/g:=}' and tested with csh - is error code kept?
+par_rpl_perlexpr_not_used_in_command() {
+    echo '### Uniq {=perlexpr=} in return - not used in command'
+    cd /tmp
+    rm -f /tmp/parallel_perlexpr.2Parallel_PerlexPr
+    echo local > parallel_perlexpr
+    parallel -Sparallel@lo --trc {=s/pr/pr.2/=}{=s/p/P/g=} echo remote OK '>' {}.2{=s/p/P/g=} ::: parallel_perlexpr
+    cat /tmp/parallel_perlexpr.2Parallel_PerlexPr
+    rm -f /tmp/parallel_perlexpr.2Parallel_PerlexPr /tmp/parallel_perlexpr
 
-echo '### functions and --nice'
-  myfunc() { echo OK $*; }; export -f myfunc; parallel --nice 10 --env myfunc -S parallel@lo myfunc ::: func
+}
 
-echo '### bug #45906: {= in header =}'
-  rm -f returnfile45906; 
-  parallel --rpl '{G} $_=lc($_)' -S parallel@lo --return {G} --cleanup echo {G} '>' {G} ::: RETURNFILE45906; 
-  ls returnfile45906
+par_remote_function_nice() {
+    echo '### functions and --nice'
+    myfunc() {
+	echo OK $*
+    }
+    export -f myfunc
+    parallel --nice 10 --env myfunc -S parallel@lo myfunc ::: func
+}
 
-echo "### bug #47608: parallel --nonall -S lo 'echo ::: ' blocks"
-  parallel --nonall -S lo 'echo ::: '
+par_rplstr_return() {
+    echo '### bug #45906: {= in header =}'
+    rm -f returnfile45906
+    parallel --rpl '{G} $_=lc($_)' -S parallel@lo --return {G} --cleanup echo {G} '>' {G} ::: RETURNFILE45906
+    ls returnfile45906
+}
 
-echo '### exported function to csh but with PARALLEL_SHELL=bash'
-  doit() { echo "$1"; }; 
-  export -f doit; 
-  stdout parallel --env doit -S csh@lo doit ::: not_OK; 
-  PARALLEL_SHELL=bash parallel --env doit -S csh@lo doit ::: OK
+par_nonall_should_not_block() {
+    echo "### bug #47608: parallel --nonall -S lo 'echo ::: ' blocks"
+    parallel --nonall -S lo 'echo ::: '
+}
 
-echo '### bug #49404: "Max jobs to run" does not equal the number of jobs specified when using GNU Parallel on remote server?'
-  echo should give 10 running jobs
-  stdout parallel -S 16/lo --progress true ::: {1..10} | grep /.10
-EOF
+par_export_functions_csh() {
+    echo '### exported function to csh but with PARALLEL_SHELL=bash'
+    doit() { echo "$1"; }
+    export -f doit
+    stdout parallel --env doit -S csh@lo doit ::: not_OK
+    PARALLEL_SHELL=bash parallel --env doit -S csh@lo doit ::: OK
+}
+
+
+par_progress_text_max_jobs_to_run() {
+    echo '### bug #49404: "Max jobs to run" does not equal the number of jobs specified when using GNU Parallel on remote server?'
+    echo should give 10 running jobs
+    stdout parallel -S 16/lo --progress true ::: {1..10} | grep /.10
+}
+
+par_hgrp_rpl() {
+    echo '### Implement {hgrp} replacement string'
+    parallel -k --plus --hgrp -S @b/bash@lo -S @c/csh@lo 'echo {sshlogin} {hgrp}' ::: b@b c@c
+}
 
 par_header_in_return() {
     echo '### bug #45907: --header : + --return {header}'
