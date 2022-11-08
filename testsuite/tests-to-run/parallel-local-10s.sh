77e9f59abb8f611d2398538ba7a8d7012a61a409
Ole Tange <ole@tange.dk>
2022-11-09 00:09:33 +0100

    parallel: --latest-line supports utf8, --colorfailed and line long --tag.

--- testsuite/tests-to-run/parallel-local-10s.sh
+++ testsuite/tests-to-run/parallel-local-10s.sh
@@ -25,15 +25,20 @@ par_totaljobs() {
 par_ll_long_line() {
     echo '### --latest-line with lines longer than terminal width'
     COLUMNS=30 parallel --delay 0.3 --tagstring '{=$_.="x"x$_=}' \
-	   --ll 'echo {}00000 | sed -e "s/$/' {1..100} /'"' ::: {1..30} |
-	perl -pe 's/.\[A//'
+	   --ll 'echo {}00000 | sed -e "s/$/' {1..100} /'"' ::: {01..30} |
+	perl -ne 's/.\[A//g;
+		  /.\[K .{4}\[m/ and next;
+		  /x\s*$/ and next;
+                  /\S/ && print'| sort -u
 }
 
 par_ll_color_long_line() {
     echo '### --latest-line --color with lines longer than terminal width'
     COLUMNS=30 parallel --delay 0.3 --color --tagstring '{=$_.="x"x$_=}' \
-	   --ll 'echo {}00000 | sed -e "s/$/' {1..100} /'"' ::: {1..30} |
-	perl -pe 's/.\[A//'
+	   --ll 'echo {}00000 | sed -e "s/$/' {1..100} /'"' ::: {01..30} |
+	perl -ne 's/.\[A//g;
+		  /.\[K .{4}\[m/ and next;
+                  /\S/ && print'| sort -u
 }
 
 par_reload_slf_every_second() {
